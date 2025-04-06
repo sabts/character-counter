fa6d117d9cea2a727a46a3434c33a5421c1c08d2
sabts <sabrinataraschi@gmail.com>
2025-04-06 13:42:56 +0200

    logic ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 const elementTextArea = document.getElementById("textarea")
 
 const elementCheckBox = document.getElementById("checkbox")
+const elementTimeReading = document.getElementById("readingtime")
 
 const elementTotalcharacters = document.getElementById("Totalcharacters")
 const elementWordCount = document.getElementById("wordcount")
 const elementSentenceCount = document.getElementById("sentencecount")
 
 //const totalCounts = document.querySelectorAll(".counter-total")
-
 let text = elementTextArea.value;
 
-
 //Primero: el contador de caracteres lee si el check box esta seleccionado(excluye los espacios)
+
 const spaceExcludeCheckBox = () => {
 let text = elementTextArea.value;
 
@@ -21,21 +21,25 @@ let text = elementTextArea.value;
     return text;
 }
 
+//Segundo, en base del primero cuente el numero de caracteres con y sin espacios
+//como cuenta: 
+//los caracteres por la cantidad del lenght
+//Word Count: por el numero de espacios.
+//sentence por el numero de enter en el teclado
+
 const charactersCounts = () => {
     let text = spaceExcludeCheckBox();
 
-    const words = text.split(/\s+/).filter(word => word.length > 0);
-    elementWordCount.textContent = words.length;
+    elementTotalcharacters.textContent = text.length;
 }
 
 const wordCounts = () => {
     let text = spaceExcludeCheckBox();
 
-    const words = text.split(/\s+/).filter(word => word.length > 0);  
+    words = text.split(/\s+/).filter(word => word.length > 0);  
     elementWordCount.textContent = words.length;
 }
     
-
 const sentenceCounts = () => {
     let text = spaceExcludeCheckBox();
 
@@ -47,30 +51,17 @@ const sentenceCounts = () => {
     }
 }
 
-elementTextArea.addEventListener('input',charactersCounts);
-elementTextArea.addEventListener('input',wordCounts );
-elementTextArea.addEventListener('input',sentenceCounts );
+const timeReading = () => {
+    let readingPerMinute = 250;
+    let text = elementTextArea.value;
+    let words = text.split(/\s+/).filter(word => word.length > 0);
+    
+    const readingTimeByWords= Math.ceil(words.length /readingPerMinute);elementTimeReading.textContent = 'Approx. reading time: '+ readingTimeByWords + ' minute';
+}
 
-elementCheckBox.addEventListener('change',charactersCounts);
+elementCheckBox.addEventListener("change",charactersCounts);
+elementTextArea.addEventListener("input", timeReading)
 
-
-//Segundo, en base del primero cuente el numero de caracteres con y sin espacios
-//como cuenta: 
-//los caracteres por la cantidad del lenght
-//Word Count: por el numero de espacios.
-//sentence por el numero de  enter en el teclado
-//const counts = () => {
-//    let text = spaceExcludeCheckBox();
-//    elementTotalcharacters.textContent = text.length;
-
-   //const words = text.split(/\s+/).filter(word => word.length > 0);  
-   // elementWordCount.textContent = words.length;
-
-   // elementSentenceCount.textContent = (text.match(/\n/g)).length; me da error por null
-//   const lineBreaks = text.match(/\n/g);
-//    if (lineBreaks === null) {
-//        elementSentenceCount.textContent = 0;
-//    } else {
-//        elementSentenceCount.textContent = lineBreaks.length;
-//    }
-//} Lo siento, hice una mega funcion primero...
+elementTextArea.addEventListener("input",charactersCounts);
+elementTextArea.addEventListener("input",wordCounts );
+elementTextArea.addEventListener("input",sentenceCounts );
